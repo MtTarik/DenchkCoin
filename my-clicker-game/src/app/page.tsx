7d6b3ebdc4eef0511212b9506404055435364c53
TarasSkorohodIO <167581010+TarasSkorohodIO@users.noreply.github.com>
2024-06-25 19:41:53 +0300

    fdроі

--- my-clicker-game/src/app/page.tsx
+++ my-clicker-game/src/app/page.tsx
@@ -1,34 +1,28 @@
 "use client"
 
-import { Button } from 'antd'; // Або інша бібліотека кнопок, яку ви використовуєте
 import MultiTouchClickerGame from '@/components/MultiTouchClickerGame';
 import Head from 'next/head';
-import { ImpactOccurredFunction, useHapticFeedback } from '@vkruglikov/react-telegram-web-app'; // Імпортуємо функції для вібрації
 import styles from './page.module.css';
+import {WebAppProvider} from "@vkruglikov/react-telegram-web-app";
 
 export default function Home() {
-    const [impactOccurred] = useHapticFeedback(); // Хук для виклику вібрації
 
-    const handleClick = () => {
-        impactOccurred('medium'); // Викликаємо вібрацію з типом 'medium'
-    };
 
     return (
-        <main className={styles.main}>
-            <div>
-                <Head>
-                    <title>Клікер Гра для Сенсорного Екрану</title>
-                    <meta name="description" content="Проста клікер гра для сенсорного екрану на Next.js" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
+        <WebAppProvider options={{ smoothButtonsTransition: true }}>
+            <main className={styles.main}>
+                <div>
+                    <Head>
+                        <title>Клікер Гра для Сенсорного Екрану</title>
+                        <meta name="description" content="Проста клікер гра для сенсорного екрану на Next.js" />
+                        <link rel="icon" href="/favicon.ico" />
+                    </Head>
 
-                <main>
-                    <MultiTouchClickerGame />
-                    <Button type="primary" onClick={handleClick}>
-                        Натисніть, щоб вібрувати
-                    </Button>
-                </main>
-            </div>
-        </main>
+                    <main>
+                        <MultiTouchClickerGame />
+                    </main>
+                </div>
+            </main>
+        </WebAppProvider>
     );
 }
